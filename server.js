a533038d72a7fd16fa7829647e24737d077ec605
jacksonmccullough <jacksonm9729@outlook.com>
2026-05-05 10:10:11 -0400

    updating smtp again

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first'); // Force IPv4 on Railway (outbound IPv6 is blocked)
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
